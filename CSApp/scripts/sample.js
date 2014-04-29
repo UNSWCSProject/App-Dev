87d36b79453d633372eebf00ec365d66203f4f34
UNSW Project <UNSWCSProject3@gmail.com>
2014-04-28 21:51:42 -0500

    Changes made by UNSW Project (UNSWCSProject3@gmail.com).

--- CSApp/scripts/sample.js
+++ CSApp/scripts/sample.js
@@ -20,15 +20,11 @@ app.openDb = function()
 }
 
 function init() {
-    window.alert("Working");
     //-----Create the database-----//
     app.openDb();
     //-----Create the Cub Information-----//
-    app.clearData();
+    //app.clearData();
     app.createTable();
-    app.insertRecord("John", "Wayne");
-    app.insertRecord("Macy", "Madeline");
-    app.insertRecord("Lee", "Hooper");
     populateCubsList();
     //-----Create User Information-----//
     //Comment out this line when you are happy to make the user data permanent
@@ -37,23 +33,36 @@ function init() {
     firstUse(); 
 }
 
-app.createTable = function() 
+
+app.testPop = function(t)
 {
-    app.db.transaction(function(tx) 
-		{
-        	tx.executeSql("CREATE TABLE IF NOT EXISTS cubTable (id INTEGER PRIMARY KEY ASC, firstName TEXT, surname TEXT)", [], 
-                          	app.onSuccess, app.onError);
-        }
-	)
-    
+    window.alert("POP: "+t);
 }
 
-app.insertRecord = function(tFirst, tLast) 
+app.createTable = function() 
 {
+    app.db.transaction(function(tx)
+		{
+            tx.executeSql("CREATE TABLE IF NOT EXISTS cubTable(cubID INTEGER PRIMARY KEY ASC, surname VARCHAR(60), firstName VARCHAR(60), "+
+            	"phone1 VARCHAR(20), phone2 VARCHAR(20), dob DATE, dateJoined DATETIME, colourSix VARCHAR(20), headOfSix TINYINT DEFAULT 0, secondOfSix "+
+                "TINYINT DEFAULT 0, dateInvested DATETIME, guardian1 VARCHAR(150), guardian2 VARCHAR(150), address VARCHAR(500), cubLevel VARCHAR(10), "+
+                "cubPosition VARCHAR(10))", [], app.onSuccess, app.onError);
+            //tx.executeSql("CREATE TABLE IF NOT EXISTS cubTable (cubID INTEGER PRIMARY KEY ASC, firstName VARCHAR(60), surname VARCHAR(60))"
+            //Kyle has replaced headOfSix 0/1 and secondOfSix 0/1 with cubPosition, the input is restricted by dropdown boxes in the build menu
+        	//window.alert("Table built");
+        }
+	)    
+}
+
+app.insertRecord = function(tFirst, tLast, tDob, tGuardian1, tPhone1, tGuardian2, tPhone2, tAddress, 
+					tCubLevel, tcubPosition, tCubColor, tDateJoined, tDateInvested) 
+{
+    window.alert("Insert called");
     app.db.transaction(function(tx) 
 		{
-        	tx.executeSql("INSERT INTO cubTable(firstName, surname) VALUES (?,?)",
-				[tFirst, tLast]);
+        	tx.executeSql("INSERT INTO cubTable(firstName, surname, dob, guardian1, phone1, guardian2, phone2, address, cubLevel, cubPosition, colourSix, "+
+                          "dateJoined, dateInvested) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)",
+				[tFirst, tLast, tDob, tGuardian1, tPhone1, tGuardian2, tPhone2, tAddress, tCubLevel, tcubPosition, tCubColor, tDateJoined, tDateInvested]);
     	}
 	)
 }
@@ -70,24 +79,24 @@ app.onError = function(tx, e)
     //window.alert("SQLite Error: " + e.message);
 }
 
-app.updateRecord = function(id, t) 
+app.updateRecord = function(cubID, t) 
 {
     app.db.transaction(function(tx) 
 		{
-        	tx.executeSql("UPDATE cubTable SET firstName = ? WHERE id = ?",
-				[t, id],
+        	tx.executeSql("UPDATE cubTable SET firstName = ? WHERE cubID = ?",
+				[t, cubID],
 				app.onSuccess,
 				app.onError);
     	}
 	);
 }
 
-app.deleteRecord = function(id) 
+app.deleteRecord = function(cubID) 
 {
     app.db.transaction(function(tx) 
 		{
-        	tx.executeSql("DELETE FROM cubTable WHERE id = ?",
-        		[id],
+        	tx.executeSql("DELETE FROM cubTable WHERE cubID = ?",
+        		[cubID],
         		app.onSuccess,
         		app.onError);
         }
@@ -98,7 +107,7 @@ app.selectAllRecords = function(fn)
 {
     app.db.transaction(function(tx) 
     	{
-            tx.executeSql("SELECT * FROM cubTable ORDER BY id", [],
+            tx.executeSql("SELECT * FROM cubTable ORDER BY cubID", [],
 			fn,
 			app.onError);
    		}
@@ -117,17 +126,16 @@ app.clearData = function()
 function populateCubsList(){
 	var render = function (tx, rs) 
     {
+        //window.alert("Populated called");
         $('#cubsList').empty();
         var len = rs.rows.length;
         //window.alert("SQLite Table: " + len + " rows found.");
         for (var i = 0; i < rs.rows.length; i++) 
         {
             var row = rs.rows.item(i);
-            $('#cubsList').append('<li><a href="#"><h3 class="ui-li-heading">'+row['firstName']+' '+row['surname']+'</a></li>');
+            $('#cubsList').append('<li><a href="#"><h3 class="ui-li-heading">'+row['firstName']+' . '+row['surname']+'</a></li>');
             //window.alert("Attempted to add to list: " + row['firstName']);
         }
- 
-        //$('#cubsList').listview();
         //window.alert("Attempted to store the query result in an array and display in listView() style");
 	}
     app.selectAllRecords(render);
